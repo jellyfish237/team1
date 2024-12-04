498fdd8d9574e9153d5a0ddaf0edece0bd610c93
MUKILTEO\1721890 <1721890@mukilteo.wednet.edu>
2024-12-04 09:40:24 -0800

    AHHH. things

--- 2D Template/Assets/oscar/Scripts/EnemyHealth.cs
+++ 2D Template/Assets/oscar/Scripts/EnemyHealth.cs
@@ -6,16 +6,21 @@ public class EnemyHealth : MonoBehaviour
 {
     public float health;
     public float maxHP;
+    private bool ghostDead;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxHP = health;
+        ghostDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0 && !ghostDead)
+        {
+            Debug.Log("Dead");
+        }
     }
 }
