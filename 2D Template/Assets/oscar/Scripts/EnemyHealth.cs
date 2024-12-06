216eab1f1588c0b2a99d66d4fe778ea7d2aa4e3b
MUKILTEO\1717481 <1717481@mukilteo.wednet.edu>
2024-12-06 09:26:06 -0800

    fixes

--- 2D Template/Assets/oscar/Scripts/EnemyHealth.cs
+++ 2D Template/Assets/oscar/Scripts/EnemyHealth.cs
@@ -7,7 +7,7 @@ public class EnemyHealth : MonoBehaviour
     public float health = 100;
     public float maxHP = 100;
     private bool ghostDead = false;
-    public float damageSpeed = 1000;
+    public float damageSpeed = 50;
     //make ghost take damage only when being chased
     [HideInInspector] public bool isInLight;
     
