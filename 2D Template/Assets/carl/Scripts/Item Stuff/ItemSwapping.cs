851836489b5f272feffea10804c5a7474be66736
1717483 <1717483@mukilteo.wednet.edu>
2024-12-19 08:59:40 -0800

    Mirrors SWAP YIPPEE

--- 2D Template/Assets/carl/Scripts/Item Stuff/ItemSwapping.cs
+++ 2D Template/Assets/carl/Scripts/Item Stuff/ItemSwapping.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ItemSwapping : MonoBehaviour
 {
+    public ItemClass[] items;
+    KeyCode cheat1 = KeyCode.F1;
+    KeyCode cheat2 = KeyCode.F2;
+    KeyCode cheat3 = KeyCode.F3;
     //Make an array of GameObject prefabs for the mirrors
     //Something to be able to press a trigger or button and swap between those items
 
@@ -11,12 +15,23 @@ public class ItemSwapping : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(cheat1))
+        {
+            GetComponent<SummonItem>().mirror = items[0];
+        }
+        if (Input.GetKeyDown(cheat2))
+        {
+            GetComponent<SummonItem>().mirror = items[1];
+        }
+        if (Input.GetKeyDown(cheat3))
+        {
+            GetComponent<SummonItem>().mirror = items[2];
+        }
     }
 }
