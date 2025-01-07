e741fb9ee2e735ae0acc11d05752edb5d09c0767
MUKILTEO\1721890 <1721890@mukilteo.wednet.edu>
2025-01-07 10:19:56 -0800

    healthbar fixed

--- 2D Template/Assets/oscar/Scripts/FloatingHealthbar.cs
+++ 2D Template/Assets/oscar/Scripts/FloatingHealthbar.cs
@@ -2,24 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class FloatingHealthbar : MonoBehaviour
+public class FloatingHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
-
-    //public GameObject enemy;
 
     public void UpdateHealthBar(float currentValue, float maxValue)
     {
         slider.value = currentValue / maxValue;
     }
-    public void Start()
-    {
-        
-        
-    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
