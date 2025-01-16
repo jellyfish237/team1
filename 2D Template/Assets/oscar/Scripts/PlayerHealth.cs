c5a05496a1e96e84644a0714fc60e3cd954df4ec
1717483 <1717483@mukilteo.wednet.edu>
2025-01-16 08:31:48 -0800

    Fist bit of dying pt2 since sourcetree is silly

--- 2D Template/Assets/oscar/Scripts/PlayerHealth.cs
+++ 2D Template/Assets/oscar/Scripts/PlayerHealth.cs
@@ -8,11 +8,15 @@ public class PlayerHealth : MonoBehaviour
     public float maxHP;
     private bool isDead;
     public ParticleSystem healthParticle;
+    private SpriteRenderer spri;
+    private Animator ani;
     // Start is called before the first frame update
     void Start()
     {
         maxHP = health;
         isDead = false;
+        ani = GetComponent<Animator>();
+        spri = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -28,6 +32,7 @@ public class PlayerHealth : MonoBehaviour
         if (isDead == true)
         {
             //Time.timeScale = 0;
+            ani.SetBool("isDead", true);
         }
     }
     public void healParticle()
